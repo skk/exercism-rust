66bce1d29c568278309e210dc5e97f53fb35023d
Steven Knight <steven.knight@cashstar.com>
2018-03-25 17:37:49 -0400

    Complete grains exercise.

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn square(s: u32) -> u64 {
-    unimplemented!();
+    match s {
+        1 ... 64 => (2 as u64).pow(s - 1),
+        _ => panic!("Square must be between 1 and 64")
+    }
 }
 
 pub fn total() -> u64 {
-    unimplemented!();
+    let values = 1..(64 + 1);
+    let val: u64 = values.into_iter().map(|x| square(x)).sum();
+    val
 }
