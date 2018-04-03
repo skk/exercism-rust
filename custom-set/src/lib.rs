edbd785f22e2f1551317950b6cce04d3ded5842f
Steven Knight <steven.knight@cashstar.com>
2018-04-02 20:11:17 -0400

    Add "add" method and add argument to new method "data".

--- custom-set/src/lib.rs
+++ custom-set/src/lib.rs
@@ -1,20 +1,33 @@
+use std::fmt::Debug;
+
 #[derive(Debug, PartialEq)]
-pub struct CustomSet<T> {
+pub struct CustomSet<T: Ord> {
     data: Vec<T>
 }
 
 
-impl<T> CustomSet<T> {
+impl<T: Ord> CustomSet<T>
+ where T: PartialEq + Clone + Debug {
     pub fn new(data: Vec<T>) -> CustomSet<T> {
-        CustomSet { data: data }
+        let mut set = CustomSet{data: vec![]};
+        for element in data {
+            set.add(element);
+        }
+        set
     }
 
     pub fn is_empty(&self) -> bool {
-        self.data.len() == 0
+        self.data.is_empty()
     }
 
-    pub fn contains(&self, c: &T) -> bool
-        where T: PartialEq {
+    pub fn contains(&self, c: &T) -> bool where T: PartialEq {
         self.data.iter().find(|&x| x == c).is_some()
+    }
+
+    pub fn add(&mut self, c: T) -> () where T: PartialEq + Clone + Debug {
+        if ! self.contains(&c) {
+            self.data.push(c);
+            self.data.sort();
+        }
     }
 }
