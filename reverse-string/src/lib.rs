d1bfb0dd9adb6920f07a03ae04bb98d194f22b70
Steven Knight <steven.knight@cashstar.com>
2018-10-13 14:23:47 -0400

    in progress

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -1,3 +1,27 @@
+
 pub fn reverse(input: &str) -> String {
-    unimplemented!("Write a function to reverse {}", input);
+    let chars = input.chars().collect::<Vec<_>>();
+    let mut data: String = String::with_capacity(input.len());
+
+    for x in chars {
+        data.insert(0, x);
+    }
+    data
 }
+
+
+//pub fn reverse(input: &str) -> String {
+//    let mut chars = input.chars().collect::<Vec<_>>();
+//    let mut data: String = String::with_capacity(input.len());
+//    while let Some(x) = chars.pop() {
+//        data.push(x);
+//    }
+//    data
+//}
+
+//fn reverse(input: &str) -> String {
+//    let s = input.to_string();
+//    let c = s.chars();
+//    let rev_str = c.rev().collect::<String>();
+//    rev_str
+//}
